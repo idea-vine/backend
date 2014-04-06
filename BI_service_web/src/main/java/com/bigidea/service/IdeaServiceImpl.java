33625c67dd7e0ef20a70023d0847ff20c0ca97f1
Partha Chandramohan <pchandramohan@aarp.org>
2014-04-05 20:49:42 -0400

    added transactional annotations

--- BI_service_web/src/main/java/com/bigidea/service/IdeaServiceImpl.java
+++ BI_service_web/src/main/java/com/bigidea/service/IdeaServiceImpl.java
@@ -3,36 +3,45 @@ package com.bigidea.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bigidea.entity.Ideas;
 import com.bigidea.entity.Users;
 import com.bigidea.hibernate.IdeaHibernate;
 
+@Service
 public class IdeaServiceImpl implements IdeaService {
 	
 	@Autowired
 	public IdeaHibernate ideaHibernate;
 
+	@Transactional(propagation=Propagation.SUPPORTS)
 	public Ideas getIdea(Integer id) {
 		return ideaHibernate.getIdeaByid(id);
 	}
 
+	@Transactional(propagation=Propagation.REQUIRED)
 	public Ideas saveIdeas(Ideas ideas) {
 		ideaHibernate.saveIdea(ideas);
 		return ideaHibernate.getIdeaByid(ideas.getId());
 	}
 
+	@Transactional(propagation=Propagation.REQUIRED)
 	public void deleteIdea(Ideas ideas) {
 		ideaHibernate.deleteIdea(ideas);
 
 	}
 
+	@Transactional(propagation=Propagation.SUPPORTS)
 	public List<Ideas> getIdeasByUser(Users users) {
 	
 		return ideaHibernate.getAllIdeasByUser(users.getId());
 		
 	}
 
+	@Transactional(propagation=Propagation.SUPPORTS)
 	public List<Ideas> getAllIdeas() {
 		return ideaHibernate.getAllIdeas();
 	}
--- BI_service_web/src/main/java/com/bigidea/service/UserServiceImpl.java
+++ BI_service_web/src/main/java/com/bigidea/service/UserServiceImpl.java
@@ -1,28 +1,36 @@
 package com.bigidea.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bigidea.entity.Users;
 import com.bigidea.hibernate.UserHibernate;
 
+@Service
 public class UserServiceImpl implements UserService {
 	
 	@Autowired
 	public UserHibernate userHibernate;
 
+	@Transactional(propagation=Propagation.REQUIRED)
 	public Users save(Users users) {
 		save(users);
 		return userHibernate.getUser(users.getId());
 	}
 
+	@Transactional(propagation=Propagation.SUPPORTS)
 	public Users get(Integer id) {
 		return userHibernate.getUser(id);
 	}
 
+	@Transactional(propagation=Propagation.SUPPORTS)
 	public Users getUserByEmail(String email) {
 		return userHibernate.getUser(email);
 	}
 
+	@Transactional(propagation=Propagation.REQUIRED)
 	public void deleteUser(Users users) {
 		deleteUser(users);
 
